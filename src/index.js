77e80b2f896be844b3fa435e4bcbce5c53830049
prizma-ru <dmitry.glotov@gmail.com>
2021-08-08 23:50:08 +0300

    fix: all errors

--- src/index.js
+++ src/index.js
@@ -1,52 +1,54 @@
 module.exports = function toReadable(number) {
     const units = ['ten', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-    const dozensTwenty = ['ten','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const dozensTwenty = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred'];
-    console.log(number%100);
+    console.log(number % 100);
+    if (number == 0) {
+        return "zero"
+    }
 
-    if (number < 11) {
+    else if (number < 11) {
         console.log("Десятки < 11");
-        return units[number%10];
+        return units[number % 10];
     }
     else if (number < 20) {
         console.log("Двацатка < 21");
-        return dozensTwenty[number - 11];
+        return dozensTwenty[number % 10];
     }
     else if (number < 100) {
-        if (number%10 == 0){
+        if (number % 10 == 0) {
             console.log("Сотка < 100");
-            return dozens[Math.floor(number/10-2)]
+            return dozens[Math.floor(number / 10 - 2)]
         }
-        else return dozens[Math.floor(number/10-2)] + " " + units[number%10];
+        else return dozens[Math.floor(number / 10 - 2)] + " " + units[number % 10];
     }
     else if (number < 1000) {
-        if (number%100 == 0){
+        if (number % 100 == 0) {
             console.log("Сотка = X00");
-            return units[Math.floor(number/100)] + " hundred"; 
+            return units[Math.floor(number / 100)] + " hundred";
         }
-        else if (number%100%10 == 0){
-            
-            if (number%100 < 11){
-                return units[Math.floor(number/100)] + " hundred " + dozensTwenty[Math.floor(number%100%10)]; 
-            }
-           else if (number%100 < 21){
-                return units[Math.floor(number/100)] + " hundred " + dozens[Math.floor(number%100%10)]; 
-            }
-            
-            else 
-            console.log("Сотка + Десятка = XX0");
+        else if (number % 100 % 10 == 0) {
 
-            return units[Math.floor(number/100)] + " hundred " + dozens[Math.floor(number%100/10-2)]; 
-        }
-        else if (number%100 < 21){
-            console.log("Сотка + <21 = XX0");
-            if (number%100 < 10){
-                return units[Math.floor(number/100)] + " hundred " + units[Math.floor(number%100%10)]; 
+            if (number % 100 < 11) {
+                return units[Math.floor(number / 100)] + " hundred " + dozensTwenty[Math.floor(number % 100 % 10)];
             }
-            else return units[Math.floor(number/100)] + " hundred " + dozensTwenty[Math.floor(number%100%10)]; 
+            else if (number % 100 < 21) {
+                return units[Math.floor(number / 100)] + " hundred " + dozens[Math.floor(number % 100 % 10)];
+            }
+
+            else
+                console.log("Сотка + Десятка = XX0");
+
+            return units[Math.floor(number / 100)] + " hundred " + dozens[Math.floor(number % 100 / 10 - 2)];
         }
-        else return units[Math.floor(number/100)] + " hundred " + dozens[Math.floor(number%100/10-2)] + " " + units[number%10];  
+        else if (number % 100 < 21) {
+            console.log("Сотка + <21 = XX0");
+            if (number % 100 < 10) {
+                return units[Math.floor(number / 100)] + " hundred " + units[Math.floor(number % 100 % 10)];
+            }
+            else return units[Math.floor(number / 100)] + " hundred " + dozensTwenty[Math.floor(number % 100 % 10)];
+        }
+        else return units[Math.floor(number / 100)] + " hundred " + dozens[Math.floor(number % 100 / 10 - 2)] + " " + units[number % 10];
     }
     else return false;
 }
-
