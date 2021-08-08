357eecd05e871de972fc4e6f690bc7580d958479
prizma-ru <dmitry.glotov@gmail.com>
2021-08-08 22:18:19 +0300

    fix: hundreds numbers

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ module.exports = function toReadable(number) {
     const dozensTwenty = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred'];
 
-
     if (number < 11) {
         return units[number];
     }
     else if (number < 20) {
         return dozensTwenty[number - 11];
     }
-    else if (number < 101) {
+    else if (number < 100) {
         if (number%10 == 0){
             return dozens[Math.floor(number/10-2)]
         }
@@ -18,11 +17,10 @@ module.exports = function toReadable(number) {
     }
     else if (number < 999) {
         if (number%100 == 0){
-            return dozens[Math.floor(number/100-1)] + "hundred";
+            return units[Math.floor(number/100)] + " hundred";
         }
         else return false;
     }
-
     else return false;
 }
 
