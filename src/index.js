bdf56decd121e30f0e37b1480118838078c171b4
prizma-ru <dmitry.glotov@gmail.com>
2021-08-08 23:21:33 +0300

    fix: code for hundreds numbers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 module.exports = function toReadable(number) {
-    const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-    const dozensTwenty = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const units = ['ten', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+    const dozensTwenty = ['ten','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred'];
     console.log(number%100);
 
     if (number < 11) {
         console.log("Десятки < 11");
-        return units[number];
+        return units[number%10];
     }
     else if (number < 20) {
         console.log("Двацатка < 21");
@@ -26,11 +26,14 @@ module.exports = function toReadable(number) {
         }
         else if (number%100%10 == 0){
             console.log("Сотка + Десятка = XX0");
-            return units[Math.floor(number/100)] + " hundred " + dozens[Math.floor(number%100/10-2)]; 
+            if (number%100 < 21){
+                return units[Math.floor(number/100)] + " hundred " + dozensTwenty[Math.floor(number%100%10)]; 
+            }
+            else return units[Math.floor(number/100)] + " hundred " + dozens[Math.floor(number%100/10-2)]; 
         }
         else if (number%100 < 21){
-            console.log("Сотка + <20 = XX0");
-            return units[Math.floor(number/100)] + " hundred " + dozensTwenty[Math.floor(number%100%10-1)]; 
+            console.log("Сотка + <21 = XX0");
+            return units[Math.floor(number/100)] + " hundred " + dozensTwenty[Math.floor(number%100%10)]; 
         }
         else return units[Math.floor(number/100)] + " hundred " + dozens[Math.floor(number%100/10-2)] + " " + units[number%10];  
     }
